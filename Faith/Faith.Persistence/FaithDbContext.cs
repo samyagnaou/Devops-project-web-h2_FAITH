2dc27e74a925910430ba3cd4e1d80bc21b1c5177
samyratchet <samy@degitise.com>
2022-08-04 01:32:42 +0200

    Persistence, dbcontext and configurations

---
+++ Faith/Faith.Persistence/FaithDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Faith.Domain.Posts;
+using Faith.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace Faith.Persistence
+{
+    public class FaithDbContext : DbContext
+    {
+        public DbSet<Post> Artworks { get; set; }
+        public DbSet<User> Users { get; set; }
+
+
+        public FaithDbContext(DbContextOptions<FaithDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(FaithDbContext).Assembly);
+        }
+
+    }
+}
